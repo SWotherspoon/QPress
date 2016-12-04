96dacd0a1538fb732e25b823e9b265a2446c3c2c
Simon Wotherspoon <Simon.Wotherspoon@utas.edu.au>
2016-12-05 00:34:02 +1100

    Add grViz support
    Deparses a model to DOT format suitable for grViz from DiagrammeR

--- R/text.R
+++ R/text.R
@@ -37,7 +37,9 @@
 ##' directed edge.
 ##'
 ##' @examples
-##' parse.text(c("A <-* B","C *-> A","C <- D","D -> C","B *--* C","A <--- D"))
+##' edges <- parse.text(c("A <-* B","C *-> A","C <- D","D -> B","B *--* C","A <--- D"))
+##' edges
+##' deparse.text(edges)
 ##' @export
 model.text <- function(file,labels=NULL) {
   parse.text(readLines(file),labels=labels)
@@ -82,9 +84,10 @@ parse.text <- function(lines,labels=NULL) {
   edges
 }
 
+
 ##' @rdname model.text
 ##' @export
-write.text <- function(edges,file="") {
+deparse.text <- function(edges) {
 
   make.edge <- function(edge) {
     edge <- edge[order(match(edge$Type,c("P","N","U","Z"),4),
@@ -102,7 +105,89 @@ write.text <- function(edges,file="") {
     paste(from," ",tail,line,head," ",to,sep="")
   }
 
-  txt <- sapply(split(edges,interaction(edges$Pair,edges$Group,drop=T)),make.edge)
+  sapply(split(edges,interaction(edges$Pair,edges$Group,drop=T)),make.edge)
+}
+
+
+##' @rdname model.text
+##' @export
+write.text <- function(edges,file="") {
+  txt <- deparse.text(edges)
   cat(txt,sep="\n",file=file)
   invisible(txt)
+}
+
+
+
+##' Write a DOT specification of the model.
+##'
+##' Write a DOT specification of the model in a form suitable for use
+##' with \code{grViz} from \pkg{DiagrammeR}.
+##'
+##' @title Export to DOT
+##' @param edges An edge list
+##' @param name The name of the digraph
+##' @param fontsize Fontsize for node labels.
+##' @param node.style The node style.
+##' @param node.shape The node shape.
+##' @param node.color The node color.
+##' @param edge.color The edge color.
+##' @return Returns a string.
+##' @export
+deparse.grViz <- function(edges,name="web",
+                          fontsize=10,node.style="filled",
+                          node.shape="oval",node.color="DarkOrange",
+                          edge.color="DarkGrey") {
+
+  make.edge <- function(edge) {
+
+    edge <- edge[order(match(edge$Type,c("P","N","U","Z"),4),
+                       edge$From),]
+
+    from <- edge$From[1]
+    to <- edge$To[1]
+    fwd <- (edge$From==from & edge$To==to)
+    ln <- c("solid","dashed","dotted","bold")
+    hd <- c("normal","dot","diamond","none")
+    tl <- c("normal","dot","diamond","none")
+    paste("    ",
+          from[1],"->",to," [",
+          "style=",ln[edge$Group[1]+1],
+          ",",
+          "arrowtail=",if(any(!fwd)) tl[match(edge$Type[!fwd],c("P","N","U","Z"))] else "none",
+          ",",
+          "arrowhead=",if(any(fwd))  hd[match(edge$Type[fwd],c("P","N","U","Z"),4)] else "none",
+          "]",
+          sep="")
+  }
+
+  ## Node definitions
+  ntxt <- paste("    ",
+                levels(edges$From),
+                " [",
+                "style=",rep(node.style,length.out=nlevels(edges$From)),
+                ",",
+                "shape=",rep(node.shape,length.out=nlevels(edges$From)),
+                ",",
+                "color=",rep(node.color,length.out=nlevels(edges$From)),
+                "]",
+                sep="",collapse="\n")
+
+  ## Edge definitions
+  etxt <- paste("    ",
+                "edge [dir=both,color=",edge.color,"]\n",
+                paste(sapply(split(edges,edges$Pair),make.edge),collapse="\n"),
+                sep="",collapse="")
+
+
+
+  ## Graph definition
+  paste("digraph ",name," {\n",
+        "    ",
+        "graph [fontsize=",fontsize,"]\n",
+        ntxt,
+        "\n\n",
+        etxt,
+        "\n}\n",
+        sep="")
 }
