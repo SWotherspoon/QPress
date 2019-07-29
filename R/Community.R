220ee3b4f3b67f2aa87cf74596fd584412edc64d
Ben Raymond <ben.raymond@aad.gov.au>
2019-07-29 13:14:44 +1000

    pkg docs to own file

--- R/Community.R
+++ R/Community.R
@@ -1,19 +1,3 @@
-#' Qualitative Network Analysis
-#'
-#' Qualitiative analysis of press perturbations of network models.
-#' Given a network model represented as a signed directed graphs, this
-#' package provide facilities for evaluating the impact of a press
-#' perturbation to the system through simulation.
-#'
-#' @name QPress-package
-#' @docType package
-#' @import XML tcltk tcltk2
-#' @author Ben Raymond, Jessica Melbourne-Thomas, Simon Wotherspoon
-NULL
-
-
-
-
 ##' Classify the sign of the elements of a vector
 ##'
 ##' Calculates the sign of the elements of then vector x, except that
---
+++ R/QPress-package.R
@@ -0,0 +1,13 @@
+#' Qualitative Network Analysis
+#'
+#' Qualitiative analysis of press perturbations of network models.
+#' Given a network model represented as a signed directed graphs, this
+#' package provide facilities for evaluating the impact of a press
+#' perturbation to the system through simulation.
+#'
+#' @name QPress-package
+#' @docType package
+#' @import XML tcltk tcltk2
+#' @importFrom shiny fluidPage radioButtons renderUI shinyApp tagList plotOutput uiOutput
+#' @author Ben Raymond, Jessica Melbourne-Thomas, Simon Wotherspoon
+NULL
